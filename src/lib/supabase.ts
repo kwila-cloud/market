78838b5a23543bcf076acb5d5e5013ea4dc421f4
Addison Emig <addison@kwila.cloud>
2025-11-18 17:27:55 -0600

    chore: remove error

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -4,9 +4,4 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = import.meta.env.PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.PUBLIC_SUPABASE_ANON_KEY;
 
-if (!supabaseUrl || !supabaseAnonKey) {
-  throw new Error('Missing Supabase environment variables');
-}
-
-console.log('supabase URL:', supabaseUrl);
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
